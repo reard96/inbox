b9665bffdabf4e276975f3047f0a117785870947
reard96 <reardon.danielj@gmail.com>
2018-06-09 13:12:31 -0400

    async/await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,17 +4,18 @@ const Web3 = require('web3'); // constructor
 
 const web3 = new Web3(ganache.provider()); // instance
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
   // Get a list of all accounts
-  web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts();
 
   // Use one of the accounts to deploy the contract
 
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {});
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
 });
