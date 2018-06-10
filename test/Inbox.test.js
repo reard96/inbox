0db93e5794bec3ef39d99c983104ab58a8d2ee5e
Dan Reardon <reardon.danielj@gmail.com>
2018-06-10 12:16:21 -0400

    next test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,12 +1,14 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor
+
 const { interface, bytecode } = require('../compile');
 
 const web3 = new Web3(ganache.provider()); // instance
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!';
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -28,4 +30,9 @@ describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
   });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_STRING);
+  })
 });
